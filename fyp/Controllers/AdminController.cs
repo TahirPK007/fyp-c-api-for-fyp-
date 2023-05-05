ecc85ce6fd087c32e83cf7537046819b70932560
Tahir Mehmood <tahir.mehmood9425@gmail.com>
2023-05-05 14:34:39 +0500

    added admin controller

---
+++ fyp/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using fyp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using fyp.Models;
+
+namespace fyp.Controllers
+{
+    public class AdminController : ApiController
+    {
+        virtualClinicEntities23 db = new virtualClinicEntities23();
+        [HttpPost]
+        public HttpResponseMessage Addnewnurse(nurse nur)
+        {
+            try
+            {
+                nurse x= new nurse();
+                x.full_name = nur.full_name;
+                x.email=nur.email;
+                x.password = nur.password;
+                x.role = "nurse";
+                db.nurses.Add(x);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, "Nurse Added Successfully");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+        [HttpPost]
+        public HttpResponseMessage Addnewsrdoc(seniorDoctor sr)
+        {
+            try
+            {
+                seniorDoctor x = new seniorDoctor();
+                x.full_name = sr.full_name;
+                x.email = sr.email;
+                x.password = sr.password;
+                x.role = "srdoc";
+                x.status = 0;
+                db.seniorDoctors.Add(x);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, "SeniorDoctor Added Successfully");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
